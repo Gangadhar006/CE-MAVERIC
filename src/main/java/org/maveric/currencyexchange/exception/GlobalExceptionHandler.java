c2bdbf7422e74713c55836782a68bfeb2f5cc7bc
gangadhar <puramgangadhar006@gmail.com>
2023-08-18 23:21:54 +0530

    updated exception handling

--- src/main/java/org/maveric/currencyexchange/exception/GlobalExceptionHandler.java
+++ src/main/java/org/maveric/currencyexchange/exception/GlobalExceptionHandler.java
@@ -11,30 +11,30 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(CustomerNotFoundException.class)
     public ResponseEntity<ErrorResponse> customerNotFound(CustomerNotFoundException exception) {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND, exception.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+        return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
     }
 
     @ExceptionHandler(AccountNotFoundException.class)
     public ResponseEntity<ErrorResponse> accountNotFound(AccountNotFoundException exception) {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND, exception.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+        return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
     }
 
     @ExceptionHandler(AccountMisMatchException.class)
     public ResponseEntity<ErrorResponse> accountMisMatch(AccountMisMatchException exception) {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_ACCEPTABLE, exception.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(errorResponse);
+        return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
     }
 
     @ExceptionHandler(InsufficientFundsException.class)
     public ResponseEntity<ErrorResponse> insufficientFunds(InsufficientFundsException exception) {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_ACCEPTABLE, exception.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(errorResponse);
+        return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
     }
 
     @ExceptionHandler(AccountConflictException.class)
     public ResponseEntity<ErrorResponse> sameAccount(AccountConflictException exception) {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_ACCEPTABLE, exception.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(errorResponse);
+        return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
     }
 }
